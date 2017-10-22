f22b5d0da11d6afbb8eec912106ee4804cf3929f
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2017-10-23 06:53:34 +0900

    HTMLアウトライン作成

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head></head>
+  <body>
+    <header>
+      <div id="header">
+        <div id="logo"></div>
+        <nav></nav>
+      </div>
+    </header>
+    <div id="cover">
+      <div class="book_status"></div>
+      <div class="book_status"></div>
+      <div class="book_status"></div>
+    </div>
+    <div id="book_list">
+      <div class="book_item">
+        <div class="book_image"></div>
+        <div class="book_detail">
+          <div class="book_title"></div>
+          <div class="book_status"></div>
+          <div class="book_status"></div>
+          <div class="book_status"></div>
+          <div class="book_delete"></div>
+        </div>
+      </div>
+    </div>
+    <footer></footer>
+  </body>
+</html>
