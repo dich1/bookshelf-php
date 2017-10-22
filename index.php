75ce368ae6bc053110341a43d719062a72221802
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2017-10-23 07:00:55 +0900

    ・タイトル、ロゴ登録ボタン、読書ステータスを追加・画像を追加

--- index.php
+++ index.php
@@ -1,17 +1,37 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head></head>
+  <head>
+    <meta charset="utf-8">
+    <title>Bookshelf | ほんだな</title>
+    <link rel="stylesheet" type="text/css" href="bookshelf.css">
+  </head>
   <body>
     <header>
       <div id="header">
-        <div id="logo"></div>
-        <nav></nav>
+        <div id="logo">
+          <a href="./bookshelf_index.php"><img src="./images/logo.png" alt="Bookshelf"></a>
+        </div>
+        <nav>
+          <a href="./bookshelf_form.php"><img src="./images/icon_plus.png" alt="">書籍登録</a>
+        </nav>
       </div>
     </header>
     <div id="cover">
-      <div class="book_status"></div>
-      <div class="book_status"></div>
-      <div class="book_status"></div>
+      <h1 id="cover_title">ほんだな</h1>
+      <form action="bookshelf_index.php" method="post">
+        <div class="book_status unread active">
+          <input type="submit" name="submit_only_unread" value="未読">
+          <div class="book_count">20</div>
+        </div>
+        <div class="book_status reading active">
+          <input type="submit" name="submit_only_reading" value="読書中">
+          <div class="book_count">23</div>
+        </div>
+        <div class="book_status finished active">
+          <input type="submit" name="submit_only_finished" value="既読">
+          <div class="book_count">14</div>
+        </div>
+      </form>
     </div>
     <div id="book_list">
       <div class="book_item">
