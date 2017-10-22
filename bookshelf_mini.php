b3e1bb4db3a917a39e9d26dc77add1c127281214
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2017-10-23 04:34:22 +0900

    ・DB接続処理追加・タイトル出力処理追加

--- bookshelf_mini.php
+++ bookshelf_mini.php
@@ -1,8 +1,39 @@
+<?php
+  // 接続に必要な情報を用意する
+  $host = 'localhost';
+  $username = 'root';
+  $password = 'root';
+  $db_name = 'bookshelf_mini';
+
+  // 接続
+  $database = mysqli_connect($host, $username, $password, $db_name);
+
+  // 失敗処理
+  if ($database == false) {
+    // mysqlエラーコード、エラー内容
+    die('Connect Error' . mysqli_connect_errno() . ')' . mysqli_connect_error());
+  }
+
+  // 文字コード設定
+  $charset = 'utf8';
+  mysqli_set_charset($database, $charset);
+
+  // SQL処理
+  // 最新のものから登録した本の情報を表示する
+  $sql = 'SELECT * FROM books ORDER BY created_at DESC';
+  $result = mysqli_query($database, $sql);
+
+  // 切断
+  mysqli_close($database);
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
     <title>Bookshelf | ほんだな</title>
+    <link rel="stylesheet" href="bookshelf.css">
   </head>
   <body>
   <?php 
@@ -20,9 +51,20 @@
     </form>
     <h2>登録書籍一覧</h2>
     <ul>
-      <?php // 登録された書籍タイトルの数だけループ ?>
-        <li><?php // print 書籍タイトル; ?></li>
-      <?php // ループ 終了 ?>
+      <?php // 登録された書籍タイトルの数だけ出力
+        if ($result) {
+          // レコードを1件取り出す
+          // 全て取り出すとfalseが返る
+          while ($record = mysqli_fetch_assoc($result)) {
+            $book_title = $record['book_title'];
+      ?>
+            <li><?php print $book_title; ?></li>
+      <?php
+          }
+          // 結果を破棄
+          mysqli_free_result($result);
+        }
+      ?>
     </ul>
   </body>
 </html>
