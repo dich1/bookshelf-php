7edaae72563ee591f938f02f5662a73aec1c8406
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2017-11-13 05:11:39 +0900

    コントローラーの呼び出し

---
+++ application/controllers/BookController.php
@@ -0,0 +1,23 @@
+<?php
+
+class BookController {
+
+    /**
+     * トップを初期表示
+     *
+     * @return array 画面に表示するデータ群
+     */
+    function index() {
+        echo "Hello";
+        // $records = $this->get_list();
+
+        // return $records;
+    }
+
+	/**
+     *
+     */
+	function register() {
+        
+	}
+}
